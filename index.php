ba7afc9e99f925ad32048ab188505bde861863cb
BrandonDo261 <minhhieudo26010@gmail.com>
2026-05-10 22:14:24 +0700

    Comment out session cookie parameters in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // 0 có nghĩa là cookie chỉ tồn tại trong phiên làm việc hiện tại
-session_set_cookie_params(0, '/TanCang/'); 
+//session_set_cookie_params(0, '/TanCang/'); 
 
 // 2. Thiết lập thời gian tự động đăng xuất sau 10 phút nếu không thao tác (tăng tính chuyên nghiệp)
 ini_set('session.gc_maxlifetime', 600);
